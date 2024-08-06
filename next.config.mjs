ce08f1fc8851604dcfa07d3faf963d5930584d2a
moon-ic <3276817499@qq.com>
2024-08-06 17:02:18 +0800

    update: 修改next版本

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,4 @@
-// next.config.mjs
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-export default {
-	webpack(config, { defaultLoaders }) {
-		config.module.rules.push({
-			test: /\.less$/,
-			use: [
-				defaultLoaders.babel,
-				{
-					loader: require("styled-jsx/webpack").loader,
-					options: {
-						type: "scoped"
-					}
-				},
-				"css-loader",
-				"less-loader"
-			]
-		});
-
-		return config;
-	}
-};
+export default nextConfig;
--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-// pages/_app.tsx
-import "../styles/globals.less";
-import { AppProps } from "next/app";
-
-const MyApp = ({ Component, pageProps }: AppProps) => {
-	return <Component {...pageProps} />;
-};
-
-export default MyApp;
